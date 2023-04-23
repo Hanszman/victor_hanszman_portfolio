7195a2d709cf50e0d97a7d9e60d1b0cab974ed0a
Hanszman <victor.hanszman@hotmail.com>
2023-04-23 16:37:41 -0300

    experiences

--- src/components/pages/experiences/Experiences.js
+++ src/components/pages/experiences/Experiences.js
@@ -1,7 +1,7 @@
 // Imports
 import './Experiences.css';
 import { useTranslation } from 'react-i18next';
-// import experiencesJson from '../../../db/experiences.json';
+import experiencesJson from '../../../db/experiences.json';
 // import projectsJson from '../../../db/projects.json';
 // import skillsJson from '../../../db/skills.json';
 
@@ -9,7 +9,7 @@ import { useTranslation } from 'react-i18next';
 function Experiences() {
     // Declarations
     const { t } = useTranslation();
-    // const jobs = experiencesJson.experiences.jobs;
+    const jobs = experiencesJson.experiences.jobs;
     // const customers = experiencesJson.experiences.customers;
     // const projects = projectsJson.projects;
     // const technologies = skillsJson.skills.technologies;
@@ -17,6 +17,27 @@ function Experiences() {
     return (
         <div>
             <h1 className='highText centerText boldText'>{t('Experiences')}</h1>
+            <div className="container">
+                <div className="row">
+                    {
+                        jobs.length > 0 && jobs.map((job) => (
+                            <div id={job.code} key={job.code} className="card cardLong">
+                                {
+                                    job.imgIcon &&
+                                    <img
+                                        className="card-img-top imgIcon"
+                                        src={require(`../../../assets/img/${job.imgIcon}`)}
+                                        alt={job.name}
+                                    />
+                                }
+                                <div className="card-body">
+                                    <h5 className="card-title">{job.companyName}</h5>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
         </div>
     );
 }
