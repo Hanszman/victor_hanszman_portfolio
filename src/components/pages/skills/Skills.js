e6da924280bf747f8ef0f2ea7fad2448c72f2b49
Hanszman <victor.hanszman@hotmail.com>
2023-05-30 16:22:43 -0300

    continuação da lógica de filtros

--- src/components/pages/skills/Skills.js
+++ src/components/pages/skills/Skills.js
@@ -1,5 +1,6 @@
 // Imports
 import './Skills.css';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import CardGroup from '../../layout/card/card-group/CardGroup';
 import Input from '../../layout/form/input/Input'
@@ -24,23 +25,44 @@ function Skills() {
         { type: 'string', detail: 'level' }
     ];
     const technologies = skillsJson.skills.technologies;
+    const [technologiesFilter, setTechnologiesFilter] = useState(technologies);
     const technologiesArrayDetails = [
         { type: 'string', detail: 'level' },
         { type: 'string', detail: 'type' },
         { type: 'string', detail: 'stack' },
         { type: 'interval', detail: 'intervalExperiencie' },
     ];
-    const levelOptions = optionsJson.options.level;
+    const levelOptions = optionsJson.options.level.filter(e => e.type === 'skills');
     const typeOptions = optionsJson.options.type;
     const stackOptions = optionsJson.options.stack;
 
     // Functions
-    function filterInput(text) {
+    function filterTechnology(event, prop) {
+        const text = event.target.value;
         console.log(text);
+        const filter = technologiesFilter.filter(obj => obj[prop].toLowerCase().includes(text.toLowerCase()));
+        console.log(filter);
+        if (text) {
+            setTechnologiesFilter(filter);
+        } else {
+            setTechnologiesFilter(technologies);
+        }
     }
 
-    function filterSelect(option) {
-        console.log(option);
+    function filterInputTechnology(event) {
+        filterTechnology(event, 'name');
+    }
+
+    function filterSelectLevel(event) {
+        filterTechnology(event, 'level');
+    }
+
+    function filterSelectType(event) {
+        filterTechnology(event, 'type');
+    }
+
+    function filterSelectStack(event) {
+        filterTechnology(event, 'stack');
     }
 
     return (
@@ -64,7 +86,7 @@ function Skills() {
             </CardGroup>
             <h2 className='text boldText'>{t('technologies')}:</h2>
             <CardGroup
-                arrayObject={technologies}
+                arrayObject={technologiesFilter}
                 arrayDetails={technologiesArrayDetails}
                 classCard='cardShort'
                 classImg='cardImgIcon'
@@ -75,25 +97,25 @@ function Skills() {
                         text={t('technology')}
                         name='technologyName'
                         placeholder={t('technology')}
-                        handleOnChange={filterInput}
+                        handleOnChange={filterInputTechnology}
                     />
                     <Select
                         text={t('level')}
                         name='technologyLevel'
                         options={levelOptions}
-                        handleOnChange={filterSelect}
+                        handleOnChange={filterSelectLevel}
                     />
                     <Select
                         text={t('type')}
                         name='technologyType'
                         options={typeOptions}
-                        handleOnChange={filterSelect}
+                        handleOnChange={filterSelectType}
                     />
                     <Select
                         text={t('stack')}
                         name='technologyStack'
                         options={stackOptions}
-                        handleOnChange={filterSelect}
+                        handleOnChange={filterSelectStack}
                     />
                 </div>
             </CardGroup>
