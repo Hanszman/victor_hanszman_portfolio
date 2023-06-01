d28a37fc1ca1195fda67f65f77e56b094b86e1ce
Hanszman <victor.hanszman@hotmail.com>
2023-06-01 09:24:05 -0300

    início da implementação de filtros nos projetos

--- src/components/pages/projects/Projects.js
+++ src/components/pages/projects/Projects.js
@@ -4,6 +4,9 @@ import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { formatList } from '../../../utils/Utils';
 import CardGroup from '../../layout/card/card-group/CardGroup';
+import Input from '../../layout/form/input/Input';
+import Select from '../../layout/form/select/Select';
+import Button from '../../layout/form/button/Button';
 import projectsJson from '../../../db/projects.json';
 import skillsJson from '../../../db/skills.json';
 import experiencesJson from '../../../db/experiences.json';
@@ -13,6 +16,7 @@ function Projects() {
     // Declarations
     const { t } = useTranslation();
     const projects = projectsJson.projects;
+    const [projectsFilter, setProjectsFilter] = useState(projects);
     const projectsArrayDetails = [
         { type: 'string', detail: 'description' },
         { type: 'date', detail: 'startDate' },
@@ -22,23 +26,72 @@ function Projects() {
     const [customersGroups, setCustomersGroups] = useState([]);
     const technologies = skillsJson.skills.technologies;
     const [technologiesGroups, setTechnologiesGroups] = useState([]);
+    const [nameFilter, setNameFilter] = useState('');
+    const [startDateFilter, setStartDateFilter] = useState('');
+    const [endDateFilter, setEndDateFilter] = useState('');
+    const [technologyFilter, setTechnologyFilter] = useState('');
+    const technologyOptions = skillsJson.skills.technologies;
 
     useEffect(() => {
         setCustomersGroups(formatList('customers', customers, '', 'customers', 'cardGroupList'));
         setTechnologiesGroups(formatList('technologies', technologies, 'stack', 'technologies', 'cardGroupList'));
     }, [customers, technologies]);
 
+    // Functions
+    function filterProject(e) {
+        e.preventDefault();
+        let filter = projects;
+        if (nameFilter)
+            filter = filter.filter(obj => obj.name.toLowerCase().includes(nameFilter.toLowerCase()));
+        console.log(startDateFilter);
+        console.log(endDateFilter);
+        if (technologyFilter)
+            filter = filter.filter(obj => obj.technologies.find(tec => tec.toLowerCase() === technologyFilter.toLowerCase()));
+        setProjectsFilter(filter);
+    }
+
     return (
         <div className='container'>
             <h1 className='highText centerText boldText'>{t('Projects')}</h1>
             <CardGroup
-                arrayObject={projects}
+                arrayObject={projectsFilter}
                 arrayDetails={projectsArrayDetails}
                 arrayLists={[customersGroups, technologiesGroups]}
                 classCard='cardLong'
                 classImg='cardimgPrint'
             >
-                {/* FILTERS */}
+                <form className='displayFlex flexWrap' onSubmit={(e) => filterProject(e)}>
+                    <Input
+                        type='text'
+                        text={t('project')}
+                        name='projectName'
+                        placeholder={t('project')}
+                        handleOnChange={e => setNameFilter(e.target.value)}
+                    />
+                    <Input
+                        type='date'
+                        text={t('startDate')}
+                        name='projectStartDate'
+                        handleOnChange={e => setStartDateFilter(e.target.value)}
+                    />
+                    <Input
+                        type='date'
+                        text={t('endDate')}
+                        name='projectEndDate'
+                        handleOnChange={e => setEndDateFilter(e.target.value)}
+                    />
+                    <Select
+                        text={t('technology')}
+                        name='projectTechnology'
+                        options={technologyOptions}
+                        handleOnChange={e => setTechnologyFilter(e.target.value)}
+                    />
+                    <div className='btnFilter'>
+                        <Button type='submit'>
+                            {t('Search')}
+                        </Button>
+                    </div>
+                </form>
             </CardGroup>
         </div>
     );
