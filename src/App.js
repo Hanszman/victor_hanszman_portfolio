cb5cdc66ded39d651c9a30c9f1bbb34061679481
Hanszman <victor.hanszman@hotmail.com>
2023-06-01 15:20:12 -0300

    organização dos TODOs

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ import Experiences from './components/pages/experiences/Experiences';
 import Skills from './components/pages/skills/Skills';
 import Projects from './components/pages/projects/Projects';
 
-// TODO: Fazer filtros na página de projeto
-// TODO: Fazer paginação e mensagem de nenhum dado encontrado com filtro inexistente
+// TODO: Fazer mensagem de nenhum dado encontrado com filtro inexistente
 // TODO: Fazer mensagem de erro de datas inconsistentes
 // TODO: Instalar biblioteca chart.js e fazer gráficos
-// TODO: Colocar links de referência entre as entidades
 // TODO: Melhorar a apresentação dos cards no css de cada página
+// TODO: Colocar links de referência entre as entidades e criar modais de visualização
 // TODO: Criar listas de links de prod e git nos cards
 // TODO: Terminar de fazer os projetos que estão inacabados, fazer o deploy deles e atualizar os dados deles nos jsons do portfólio
 // TODO: Colocar nos README de todos os projetos as versões das tecnologias para facilitar a instação
+// TODO: Talvez fazer paginação
 // TODO: Talvez ordenar por prioridade os agrupamentos na função formatList
 // TODO: Talvez melhorar agrupamento da formatList caso o usuário queira mudar agrupamentos de type ou stack ou os dois juntos por exemplo
 // TODO: Talvez inserir no json de skills os padrões de projeto como POO e MVC por exemplo
